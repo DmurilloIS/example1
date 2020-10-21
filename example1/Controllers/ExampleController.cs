2b3b86ed5fe44bdb2a30aee87f9f76eecdcab6d7
Diego Murillo Rodríguez <diego.murillo.rod@gmail.com>
2020-10-20 18:55:19 -0600

    Agregar archivos de proyecto.

---
+++ example1/Controllers/ExampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace example1.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ActionResult Index()
+        {
+            int num1 = 120;
+
+            if (num1 > 500)
+            {
+
+            }
+            return View();
+        }
+    }
+}
